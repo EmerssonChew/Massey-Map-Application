94283deef4f2687a0b7db43966bd5d75f30746bc
Emersson Chew <emerssonchewcs@gmail.com>
2017-05-07 21:41:24 +1200

    Started connecting the app with the Mapbox API. Stuck on MapboxMap section on https://www.mapbox.com/help/first-steps-android-sdk/#mapView

--- app/src/main/java/com/example/a158383/masseymapapp/Map.java
+++ app/src/main/java/com/example/a158383/masseymapapp/Map.java
@@ -2,13 +2,14 @@ package com.example.a158383.masseymapapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.mapbox.mapboxsdk.Mapbox;
 
 public class Map extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, getString(R.string.access_token));
         setContentView(R.layout.activity_map);
-
     }
 }
