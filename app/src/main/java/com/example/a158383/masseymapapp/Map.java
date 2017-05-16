5a08207a6e490fdb3815ef40d141ac0cc9881a00
Emersson Chew <emerssonchewcs@gmail.com>
2017-05-16 19:14:23 +1200

    Successfully integrated a static map view into the app with the side drawer and buttons visible. However, changing the map view to dynamic means that the side drawer and buttons are invisible. The map view is currently dynamic. Need to have dynamic map view with buttons visible.

--- app/src/main/java/com/example/a158383/masseymapapp/Map.java
+++ app/src/main/java/com/example/a158383/masseymapapp/Map.java
@@ -1,15 +1,91 @@
 package com.example.a158383.masseymapapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.constants.Style;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.MapboxMapOptions;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 
 public class Map extends AppCompatActivity {
 
+    private MapView mapView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         Mapbox.getInstance(this, getString(R.string.access_token));
-        setContentView(R.layout.activity_map);
+
+        //Dynamic mapView
+        MapboxMapOptions options = new MapboxMapOptions()
+                .styleUrl(Style.OUTDOORS)
+                .camera(new CameraPosition.Builder()
+                        .target(new LatLng(-36.7323894, 174.70274110000003))
+                        .zoom(15)
+                        .build());
+
+        //(R.layout.activity_map); //Static mapview
+
+        //Create a mapView
+        //mapView = (MapView) findViewById(R.id.mapView); //Static mapView
+        mapView = new MapView(this, options); //Dynamic mapView
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+                // Customize map with markers, polylines, etc.
+            }
+        });
+
+        setContentView(mapView); //Dynamic mapView
+    }
+
+    //Add the mapView lifecycle to the activity's lifecycle methods
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
     }
 }
